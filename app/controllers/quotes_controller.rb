cb280b7402a27f49b334b1e62347c7acf284e5d5
Gustavo Morales <gustavo.morales@gmail.com>
2017-06-29 19:17:44 -0500

    Create and Read All Quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,13 @@
 get '/quotes' do
+  @quotes = @@quotes
   erb :'quotes/index'
+end
+
+get '/quotes/new' do
+  erb :'quotes/new'
+end
+
+post '/quotes' do
+  @@quotes.push({ description: params[:description], author: params[:author] })
+  redirect '/quotes'
 end
