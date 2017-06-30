7f478de982e3e063a0771d8b01a6743cc82ab481
Gustavo Morales <gustavo.morales@gmail.com>
2017-06-29 20:19:46 -0500

    Edit and Delete Quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -13,7 +13,25 @@ post '/quotes' do
 end
 
 get '/quotes/:id' do
-  index = params[:id].to_i
-  @q = @@quotes[index]
+  @index = params[:id].to_i
+  @q = @@quotes[@index]
   erb :'quotes/show'
+end
+
+get '/quotes/:id/edit' do
+  @index = params[:id].to_i
+  @q = @@quotes[@index]
+  erb :'quotes/edit'
+end
+
+post '/quotes/:id/update' do
+  @index = params[:id].to_i
+  @@quotes[@index] = { description: params[:description], author: params[:author] }
+  redirect '/quotes'
+end
+
+get '/quotes/:id/delete' do
+  @index = params[:id].to_i
+  @@quotes.delete_at(@index)
+  redirect '/quotes'
 end
